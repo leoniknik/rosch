cba53ad2a300ea77609e7870c80a8cd4be66d25f
Evtuhov <Evtuhov@binbank.ru>
2018-11-17 17:23:56 +0300

    initial dialog state parser

--- Roschat/Roschat/Classes/Model/Dto/DialogStateDto.swift
+++ Roschat/Roschat/Classes/Model/Dto/DialogStateDto.swift
@@ -7,7 +7,38 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+enum DialogStateType: String {
+    case button = "BUTTON"
+    case form = "FORM"
+    case text = "TEX"
+    case doc = "DOC"
+    case photo = "PHOTO"
+    case undefined
+}
+
+enum DialogMessageStyle: String {
+    case normal
+}
+
+struct DialogMessageDto {
+    var body: JSON
+    
+    func getMessage(_ style: DialogMessageStyle) -> String {
+        return body[style.rawValue].string ?? body[DialogMessageStyle.normal.rawValue].string ?? "ОШИБКА ВАРИАНТА ДИАЛОГА"
+    }
+}
 
 class DialogStateDto {
+    var id: Int
+    var type: DialogStateType
     
+    var dialogStyleType: DialogMessageStyle
+    
+    init(json: JSON) {
+        self.id = json["id"].intValue
+        self.type = DialogStateType(rawValue: json["type"].stringValue) ?? .undefined
+        self.dialogStyleType = DialogMessageStyle(rawValue: json["style"].stringValue) ?? .normal
+    }
 }
